fb48539ba0b8e84cb3672afe58829ff0ff6d081d
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-13 17:53:11 -0400

    id in different format

--- Session1.py
+++ Session1.py
@@ -4,4 +4,7 @@
 instagram_user_name ="diksha"
 
 #Read operation
-print(instagram_user_name)
+print(instagram_user_name, id(instagram_user_name))  #id gives the memory location
+print(instagram_user_name, hex(id(instagram_user_name)))
+print(instagram_user_name, oct(id(instagram_user_name)))
+print(instagram_user_name, bin(id(instagram_user_name)))
