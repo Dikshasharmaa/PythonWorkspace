64f1ae0a4e95c91284e7f0c61a1da6f64f36eec2
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-14 16:48:11 -0400

    Multi Value Container

---
+++ Session1A.py
@@ -0,0 +1,12 @@
+# Multi Value Container
+
+#Create operation -> Homogeneous
+numbers = 10, 20, 30, 40
+
+#Read operation
+print(numbers, id (numbers), type(numbers))
+
+#Create Operation -> Hetrogeneous
+data = (10, 10.2, "hello", "wow", 200)
+print(data, id(data), type(data))
+print(data[0], id(data[0]), type(data[0]))
