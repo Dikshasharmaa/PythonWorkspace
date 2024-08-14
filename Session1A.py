1152c7563d052389c5953d0cba79b8087bec1aaa
Diksha Sharma <diksha66sharma78@gmail.com>
2024-08-14 17:03:46 -0400

    Reference Copy Operation

--- Session1A.py
+++ Session1A.py
@@ -10,3 +10,10 @@ print(numbers, id (numbers), type(numbers))
 data = (10, 10.2, "hello", "wow", 200)
 print(data, id(data), type(data))
 print(data[0], id(data[0]), type(data[0]))
+print(data[1], id(data[1]), type(data[1]))
+print(data[2], id(data[2]), type(data[2]))
+
+#Reference Copy Operation
+my_data= data
+print(my_data, id(my_data))
+
